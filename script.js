9888df04418edfdc77407de1e256c564d225c78e
Adam3362 <113584167+Adam3362@users.noreply.github.com>
2022-12-08 08:47:25 +0100

    bbb

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ var container = document.getElementById('body');
 var val = e.value;
 container.setAttribute("style", "filter: brightness("+val+"%);");
 }
+
+const splash = document.querySelector('.splash');
+
+document.addEventListener('DOMContentLoaded', (e)=>{
+setTimeout( ()=>{
+
+    splash.classList.add('display-none');
+},2000)
+
+})
+
